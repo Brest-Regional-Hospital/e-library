7ef94b75e5826a929c616117a617553ee46b6606
Hardzei Stakhavets <gordeystahovets@gmail.com>
2024-06-13 22:02:53 +0300

    feat(layout): update grid layout for catalog page

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -4,18 +4,53 @@ import BookCard from 'widgets/Card/ui/BookCard';
 import NewspaperCard from 'widgets/Card/ui/NewspaperCard';
 import MagazineCard from 'widgets/Card/ui/MagazineCard';
 import { Filters } from 'widgets/Filters/ui/Filters';
-import { Card, Grid, styled } from '@mui/material';
+import { Box, Card, Grid, Typography, styled } from '@mui/material';
+import ArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const FiltersCard = styled(Card)(({ theme }) => ({
-    width: '350px',
     padding: '20px',
     alignSelf: 'start',
-    marginRight: '20px',
-    [theme.breakpoints.down('lg')]: {
-        width: '500px',
-    },
+    flexBasis: '30%',
     [theme.breakpoints.down(1000)]: {
         display: 'none',
+    },
+    [theme.breakpoints.down(1350)]: {
+        flexBasis: '40%',
+    },
+}));
+
+const PageTitleTypography = styled(Typography)(({ theme }) => ({
+    fontWeight: 500,
+    fontSize: 28,
+}));
+
+const CatalogContainer = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: '1450px',
+    margin: `${theme.spacing(6)} auto 0 auto`,
+    padding: '0 40px',
+    [theme.breakpoints.down(900)]: {
+        marginTop: theme.spacing(4),
+    },
+}));
+
+const CatalogGridItem = styled(Grid)(({ theme }) => ({
+    [theme.breakpoints.down(1000)]: {
+        width: '100%',
+    },
+
+    [theme.breakpoints.between(700, 1000)]: {
+        width: '47%',
+    },
+}));
+
+const CatalogTitleIcon = styled(Typography)(({ theme }) => ({
+    display: 'none',
+
+    [theme.breakpoints.down(1000)]: {
+        display: 'block',
+        marginTop: '8px',
     },
 }));
 
@@ -23,46 +58,62 @@ export const App = () => {
     return (
         <div className="app">
             <Navbar />
-
-            <div style={{ display: 'flex', marginTop: '50px' }}>
-                <Grid
-                    justifyContent="center"
-                    container
-                    md={14}
-                    sm={14}
-                    lg={9}
-                    xl={9}
-                    spacing={6}
+            <CatalogContainer>
+                <div
+                    role="button"
+                    onClick={() => console.log(1)}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '5px',
+                        cursor: 'pointer',
+                    }}
                 >
-                    <Grid item>
-                        <BookCard />
+                    <PageTitleTypography variant="h3">
+                        Весь каталог
+                    </PageTitleTypography>
+                    <CatalogTitleIcon variant="body1">
+                        <ArrowDownIcon color="primary" />
+                    </CatalogTitleIcon>
+                </div>
+
+                <div
+                    style={{
+                        display: 'flex',
+                        marginTop: '30px',
+                    }}
+                >
+                    <Grid container md={14} sm={14} lg={14} xl={12} gap="40px">
+                        <CatalogGridItem item>
+                            <BookCard />
+                        </CatalogGridItem>
+                        <CatalogGridItem item>
+                            <BookCard />
+                        </CatalogGridItem>
+                        <CatalogGridItem item>
+                            <BookCard />
+                        </CatalogGridItem>
+                        <CatalogGridItem item>
+                            <BookCard />
+                        </CatalogGridItem>
+                        <CatalogGridItem item>
+                            <BookCard />
+                        </CatalogGridItem>
+                        <CatalogGridItem item>
+                            <BookCard />
+                        </CatalogGridItem>
+                        <CatalogGridItem item>
+                            <BookCard />
+                        </CatalogGridItem>
+                        <CatalogGridItem item>
+                            <BookCard />
+                        </CatalogGridItem>
                     </Grid>
-                    <Grid item>
-                        <BookCard />
-                    </Grid>
-                    <Grid item>
-                        <BookCard />
-                    </Grid>
-                    <Grid item>
-                        <BookCard />
-                    </Grid>
-                    <Grid item>
-                        <BookCard />
-                    </Grid>
-                    <Grid item>
-                        <BookCard />
-                    </Grid>
-                    <Grid item>
-                        <BookCard />
-                    </Grid>
-                    <Grid item>
-                        <BookCard />
-                    </Grid>
-                </Grid>
-                <FiltersCard>
-                    <Filters />
-                </FiltersCard>
-            </div>
+                    <FiltersCard>
+                        <Filters />
+                    </FiltersCard>
+                </div>
+            </CatalogContainer>
         </div>
     );
 };
